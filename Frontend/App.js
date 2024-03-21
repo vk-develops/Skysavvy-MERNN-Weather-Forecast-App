070876cece59eeb070c12af04a3bac88f3ad43c1
vk-develops <vimalkumarv291@gmail.com>
2024-03-21 20:54:38 +0530

    added the statusbar and safeareaview

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,13 +1,19 @@
 import { StatusBar } from "expo-status-bar";
+import { SafeAreaView } from "react-native-safe-area-context";
 import { Text, View } from "react-native";
 
 export default function App() {
     return (
-        <View>
-            <Text className="text-3xl pt-10 text-blue-400">
-                Open up App.js to start working on your app!
-            </Text>
-            <StatusBar style="auto" />
-        </View>
+        <SafeAreaView>
+            <View>
+                <Text className="text-3xl pt-10 text-blue-400">
+                    Open up App.js to start working on your app!
+                </Text>
+                <StatusBar
+                    backgroundColor="#23227B"
+                    style="light"
+                />
+            </View>
+        </SafeAreaView>
     );
 }
