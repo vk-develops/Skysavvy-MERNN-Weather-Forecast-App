2a26a3bfd55d26061128ca1282470bbe9c93b8a8
vk-develops <vimalkumarv291@gmail.com>
2024-03-21 21:33:20 +0530

    added the intial header and image on the onboarding screen

--- Frontend/src/Pages/Onboarding/OnboardingScreen.js
+++ Frontend/src/Pages/Onboarding/OnboardingScreen.js
@@ -1,6 +1,7 @@
-import { View, Text, ScrollView } from "react-native";
+import { View, Text, ScrollView, Image } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import React from "react";
+import OnboardingImg from "../../../assets/Images/OnboardingImg.png";
 
 const OnboardingScreen = () => {
     return (
@@ -12,7 +13,23 @@ const OnboardingScreen = () => {
                 }}
                 className="flex flex-1 h-screen"
                 colors={["#23227B", "#000236"]}
-            ></LinearGradient>
+            >
+                <View className="px-4">
+                    <Image
+                        source={OnboardingImg}
+                        className="mt-4 w-full"
+                    />
+                    <View>
+                        <Text
+                            className="text-[30px] text-white capitalize text-center"
+                            style={{ fontFamily: "jakartaSemiBold" }}
+                        >
+                            {" "}
+                            weather insights with SkySavvy's accuracy.
+                        </Text>
+                    </View>
+                </View>
+            </LinearGradient>
         </ScrollView>
     );
 };
