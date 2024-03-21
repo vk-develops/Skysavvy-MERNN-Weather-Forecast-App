3fb9540106e11fa8bc0bfee7d8fcc7ed732053ec
vk-develops <vimalkumarv291@gmail.com>
2024-03-21 21:04:28 +0530

    chore: added the new react navigation packages and initialozed the new onboarding screen

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,6 +1,5 @@
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Text, View } from "react-native";
 
 export default function App() {
     return (
