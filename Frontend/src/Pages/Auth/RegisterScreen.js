d4039dcb865357fc1f4c4e89f5069c4d4e7c412f
vk-develops <vimalkumarv291@gmail.com>
2024-03-21 22:59:30 +0530

    feat: Added the background image for the register screen

--- Frontend/src/Pages/Auth/RegisterScreen.js
+++ Frontend/src/Pages/Auth/RegisterScreen.js
@@ -1,11 +1,27 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView, ImageBackground } from "react-native";
 import React from "react";
+import LoginBg from "../../../assets/Images/Login-Bg.png";
+import { LinearGradient } from "expo-linear-gradient";
 
 const RegisterScreen = () => {
     return (
-        <View>
-            <Text>RegisterScreen</Text>
-        </View>
+        <ScrollView>
+            <LinearGradient
+                contentContainerStyle={{
+                    alignItems: "center",
+                    justifyContent: "center",
+                }}
+                className="flex flex-1 h-screen"
+                colors={["#23227B", "#000236"]}
+            >
+                <ImageBackground
+                    imageStyle={{ opacity: 0.4 }}
+                    source={LoginBg}
+                    resizeMode="cover"
+                    className="flex-1 -m-12 justify-center"
+                ></ImageBackground>
+            </LinearGradient>
+        </ScrollView>
     );
 };
 
