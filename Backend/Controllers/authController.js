67f7d6c09470e36dacde2ce6ea729244c98182bf
vk-develops <vimalkumarv291@gmail.com>
2024-03-23 19:59:34 +0530

    feat: Added the register controller

--- Backend/Controllers/authController.js
+++ Backend/Controllers/authController.js
@@ -1,1 +1,55 @@
 import asyncHandler from "express-async-handler";
+import bcrypt from "bcryptjs";
+import User from "../Models/userModel.js";
+
+// @desc    Register users & and get a token
+// @route   POST /api/v1/users/auth/register
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+
+        //Check for all fields
+        if (!name || !email || !password) {
+            return res
+                .status(400)
+                .json({ success: false, message: "All fields are neccessary" });
+        }
+
+        //Check for existing user
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res
+                .status(400)
+                .json({ success: false, message: "Already a user" });
+        }
+
+        //Hashing the password
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        //Storing the user in the database
+        const user = await User.create({
+            name,
+            email,
+            password: hashedPassword,
+        });
+
+        if (user) {
+            //Destructuring the user details
+            const { password, ...resetofUserDetails } = user._doc;
+
+            //Sending Resopnse
+            res.status(200).json({
+                success: true,
+                message:
+                    "User registration sucesss, Account verification OTP send to your mail",
+                data: resetofUserDetails,
+            });
+        }
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).json({ success: false, err: err.message });
+    }
+});
+
+export { registerUser, loginUser, logoutUser };
