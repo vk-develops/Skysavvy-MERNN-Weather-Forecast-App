28ca9850d78f71fb645cb80f5f9586028d42e8ea
vk-develops <vimalkumarv291@gmail.com>
2024-03-24 17:49:58 +0530

    feat: Added the complete register api with OTP verifiaction

--- Backend/Routes/accountRoute.js
+++ Backend/Routes/accountRoute.js
@@ -1,10 +1,12 @@
 import express from "express";
+import { verifyAccount } from "../Controllers/accountController.js";
+import { protect } from "../Middlewares/authMiddleware.js";
 
 // router init
 const router = express.Router();
 
 // HTTP Methods for account verification
-router.post("/verify");
+router.post("/verify", protect, verifyAccount);
 router.get("/resend-otp");
 
 //HTTP Methods for password reset
