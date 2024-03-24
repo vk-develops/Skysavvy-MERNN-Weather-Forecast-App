c98e9d588bb200a2b924db0f1886f498b39269fd
vk-develops <vimalkumarv291@gmail.com>
2024-03-24 14:03:20 +0530

    feat: Added the mail gnerator hosts and services

--- Backend/Utils/accountVerifiactionUtil.js
+++ Backend/Utils/accountVerifiactionUtil.js
@@ -12,5 +12,25 @@ const generateOTP = () => {
     return OTP;
 };
 
+//Setting up the mailgen environment
+const Mailgenerator = new Mailgen({
+    theme: "default",
+    product: {
+        name: "MovieMatic",
+        link: "https://vk-develops.vercel.app",
+    },
+});
+
+const mailTransport = () => {
+    //Mail Host
+    return nodeMailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: process.env.GMAIL_EMAIL_ID,
+            pass: process.env.GMAIL_EMAIL_PASSWORD,
+        },
+    });
+};
+
 //Exports
-export { generateOTP };
+export { generateOTP, Mailgenerator, mailTransport };
