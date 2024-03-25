2b9715121f9047ee426bee879a7a53fba6d065ea
vk-develops <vimalkumarv291@gmail.com>
2024-03-25 20:44:18 +0530

    feat: Added the logout and login api

--- Backend/Routes/authRoute.js
+++ Backend/Routes/authRoute.js
@@ -1,5 +1,10 @@
 import express from "express";
-import { loginUser, registerUser } from "../Controllers/authController.js";
+import {
+    loginUser,
+    logoutUser,
+    registerUser,
+} from "../Controllers/authController.js";
+import { protect } from "../Middlewares/authMiddleware.js";
 
 // router init
 const router = express.Router();
@@ -7,7 +12,7 @@ const router = express.Router();
 // HTTP Methods
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-router.post("/logout");
+router.post("/logout", protect, logoutUser);
 
 // Export
 export default router;
