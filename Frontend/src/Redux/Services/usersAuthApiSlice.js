5c17ce0825098bf483d8f5f253eac3cbc2c2fd9d
vk-develops <vimalkumarv291@gmail.com>
2024-03-26 20:19:00 +0530

    feat: Added the login query

--- Frontend/src/Redux/Services/usersAuthApiSlice.js
+++ Frontend/src/Redux/Services/usersAuthApiSlice.js
@@ -14,7 +14,19 @@ export const usersAuthApiSlice = apiSlice.injectEndpoints({
                 body: data,
             }),
         }),
+
+        login: builder.mutation({
+            query: (data) => ({
+                url: `${AUTH_URL}login`,
+                method: "POST",
+                credentials: "include",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: data,
+            }),
+        }),
     }),
 });
 
-export const { useRegisterMutation } = usersAuthApiSlice;
+export const { useRegisterMutation, useLoginMutation } = usersAuthApiSlice;
