74707baa89d88ddfad9a190e6229fd0d0d7c449f
vk-develops <vimalkumarv291@gmail.com>
2024-03-26 20:30:03 +0530

    feat: Added the redux store

--- Frontend/src/Redux/store.js
+++ Frontend/src/Redux/store.js
@@ -1,5 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { apiSlice } from "./Service/apiSlice";
+import usersAuthSlice from "./Features/usersAuthSlice";
 
-const store = configureStore({});
+const store = configureStore({
+    reducer: {
+        [apiSlice.reducerPath]: apiSlice.reducer,
+        auth: usersAuthSlice,
+    },
+
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(apiSlice.middleware),
+    devTools: false,
+});
 
 export default store;
