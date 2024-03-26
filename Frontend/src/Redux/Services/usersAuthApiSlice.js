6e718d6cb4bbec0cb45a38e1b135c90198845e35
vk-develops <vimalkumarv291@gmail.com>
2024-03-26 20:16:56 +0530

    feat: Added the RTK query for the register endpoint

---
+++ Frontend/src/Redux/Services/usersAuthApiSlice.js
@@ -0,0 +1,20 @@
+import { AUTH_URL } from "../constants";
+import { apiSlice } from "./apiSlice";
+
+export const usersAuthApiSlice = apiSlice.injectEndpoints({
+    endpoints: (builder) => ({
+        register: builder.mutation({
+            query: (data) => ({
+                url: `${AUTH_URL}register`,
+                method: "POST",
+                credentials: "include",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: data,
+            }),
+        }),
+    }),
+});
+
+export const { useRegisterMutation } = usersAuthApiSlice;
