3527ebbfb81815c25b5c96b38d7c8781a5b2eaf8
vk-develops <vimalkumarv291@gmail.com>
2024-03-28 21:19:13 +0530

    feat: Added the logout functionality

--- Frontend/src/Pages/Home/HomeScreen.js
+++ Frontend/src/Pages/Home/HomeScreen.js
@@ -1,8 +1,25 @@
-import { ScrollView, Text, View } from "react-native";
+import { Button, ScrollView, View } from "react-native";
 import React from "react";
 import { LinearGradient } from "expo-linear-gradient";
+import { useLogoutMutation } from "../../Redux/Services/usersAuthApiSlice";
+import { useDispatch } from "react-redux";
+import { removeCredentials } from "../../Redux/Features/usersAuthSlice";
 
 const HomeScreen = () => {
+    const dispatch = useDispatch();
+
+    const [logout] = useLogoutMutation();
+
+    const logoutHandler = async () => {
+        try {
+            const response = await logout().unwrap();
+            console.log(response);
+            dispatch(removeCredentials());
+        } catch (err) {
+            console.log(err.message);
+        }
+    };
+
     return (
         <ScrollView className="flex-1">
             <LinearGradient
@@ -14,7 +31,10 @@ const HomeScreen = () => {
                 colors={["#23227B", "#000236"]}
             >
                 <View>
-                    <Text>Hello home page</Text>
+                    <Button
+                        title="Logout"
+                        onPress={logoutHandler}
+                    />
                 </View>
             </LinearGradient>
         </ScrollView>
