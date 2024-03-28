05d592f3f260f49eacf7a21b4c999270a8a5198f
vk-develops <vimalkumarv291@gmail.com>
2024-03-28 19:34:35 +0530

    feat: Added the check isloggedin functionality

--- Frontend/src/Navigation/MainNavigator.js
+++ Frontend/src/Navigation/MainNavigator.js
@@ -1,10 +1,35 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import DrawerNavigator from "./DrawerNavigator";
 import OnboardingStack from "../Pages/Onboarding/OnboardingStack";
+import { AUTH_URL, BASE_URL } from "../Redux/constants";
+import { setCredentials } from "../Redux/Features/usersAuthSlice";
 
 const MainNavigator = () => {
     const { isAuthenticated } = useSelector((state) => state.auth);
+
+    const dispatch = useDispatch();
+
+    const checkIsLoggedIn = async () => {
+        try {
+            const response = await fetch(`${BASE_URL}${AUTH_URL}/isloggedin`, {
+                method: "GET",
+                credentials: "include",
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                const userInfo = data.userInfo;
+                dispatch(setCredentials(userInfo));
+            }
+        } catch (err) {
+            console.log(err.message);
+        }
+    };
+
+    useEffect(() => {
+        checkIsLoggedIn();
+    });
 
     return isAuthenticated ? <DrawerNavigator /> : <OnboardingStack />;
 };
