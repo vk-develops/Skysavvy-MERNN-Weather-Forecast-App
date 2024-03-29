bf9f0945d85557599857fba4000406e4d23f71a8
vk-develops <vimalkumarv291@gmail.com>
2024-03-29 20:38:47 +0530

    feat: Added the complete account verification UI

--- Frontend/src/Pages/Auth/AccountVerificationScreen.js
+++ Frontend/src/Pages/Auth/AccountVerificationScreen.js
@@ -1,8 +1,15 @@
-import { View, Text, ScrollView, ImageBackground } from "react-native";
+import {
+    View,
+    Text,
+    ScrollView,
+    ImageBackground,
+    TouchableOpacity,
+} from "react-native";
 import React from "react";
 import { LinearGradient } from "expo-linear-gradient";
 import { OtpInput } from "react-native-otp-entry";
 import LoginBg from "../../../assets/Images/Login-Bg.png";
+import { styles } from "../../Styles/style";
 
 const AccountVerificationScreen = () => {
     return (
@@ -19,12 +26,65 @@ const AccountVerificationScreen = () => {
                     imageStyle={{ opacity: 0.4 }}
                     source={LoginBg}
                     resizeMode="cover"
-                    className="flex-1 -m-12 justify-center"
+                    className="flex-1 -m-12 "
                 >
-                    <OtpInput
-                        numberOfDigits={6}
-                        onTextChange={(text) => console.log(text)}
-                    />
+                    <View className="m-12 pt-28 px-4">
+                        <View>
+                            <Text
+                                className="text-[30px] text-white text-center pt-6"
+                                style={{ fontFamily: "plexSemiBold" }}
+                            >
+                                Verify Account
+                            </Text>
+                            <Text
+                                className={`${styles.paraText} pt-[22px]`}
+                                style={{ fontFamily: "plexRegular" }}
+                            >
+                                Install Bootstrap’s source Sass and JavaScript
+                                files via npm or Meteor. Package managed
+                                installs don’t full build scripts.
+                            </Text>
+                            <View className="mt-16">
+                                <OtpInput
+                                    numberOfDigits={6}
+                                    style={{ width: "100%", height: 50 }}
+                                    focusColor="#FACC15"
+                                    focusStickBlinkingDuration={500}
+                                    theme={{
+                                        pinCodeTextStyle: { color: "white" },
+                                    }}
+                                    onTextChange={(text) => console.log(text)}
+                                    onFilled={(text) =>
+                                        console.log(`OTP is ${text}`)
+                                    }
+                                />
+                            </View>
+                            <View className="flex items-center justify-end gap-[1px] flex-row mt-8">
+                                <Text
+                                    className="text-slate-300 text-base"
+                                    style={{ fontFamily: "plexRegular" }}
+                                >
+                                    Did'nt recieve OTP ?{" "}
+                                </Text>
+
+                                <TouchableOpacity
+                                    onPress={() =>
+                                        navigation.navigate("RegisterScreen")
+                                    }
+                                    activeOpacity={0.7}
+                                >
+                                    <Text
+                                        className=" text-yellow-400 text-base"
+                                        style={{
+                                            fontFamily: "plexMedium",
+                                        }}
+                                    >
+                                        Resend OTP
+                                    </Text>
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+                    </View>
                 </ImageBackground>
             </LinearGradient>
         </ScrollView>
