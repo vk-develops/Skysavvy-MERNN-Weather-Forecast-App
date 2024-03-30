d84cf5ad516e8d40c463266d623b86c5920c66a7
vk-develops <vimalkumarv291@gmail.com>
2024-03-30 20:53:44 +0530

    feat: Added the weatherdata based on the users location

--- Frontend/src/Pages/Home/HomeScreen.js
+++ Frontend/src/Pages/Home/HomeScreen.js
@@ -7,6 +7,68 @@ import Header from "../../Components/Header";
 import MiniWeatherCard from "../../Components/MiniWeatherCard";
 
 const HomeScreen = ({ navigation }) => {
+    const [location, setLocation] = useState(null);
+    const [address, setAddress] = useState(null);
+    const [city, setCity] = useState(null);
+    const [weatherData, setWeatherData] = useState(null);
+
+    const getLocationPermission = async () => {
+        let { status } = await Location.requestForegroundPermissionsAsync();
+
+        if (status !== "granted") {
+            console.log("Location not granted");
+            setCity("Mumbai");
+        } else {
+            let getlocation = await Location.getCurrentPositionAsync({});
+            setLocation(getlocation);
+        }
+    };
+
+    const reverseGeo = async () => {
+        if (location) {
+            const { coords } = location;
+            const reverseGeoCodeAddress = await Location.reverseGeocodeAsync({
+                latitude: coords.latitude,
+                longitude: coords.longitude,
+            });
+            setAddress(reverseGeoCodeAddress);
+
+            if (reverseGeoCodeAddress) {
+                setCity(reverseGeoCodeAddress[0].city);
+            }
+        } else {
+            console.log("Location is null or undefined");
+        }
+    };
+
+    const getWeatherData = async () => {
+        try {
+            const response = await fetch(
+                `${process.env.EXPO_PUBLIC_WEATHER_API_CURRENT}?key=${process.env.EXPO_PUBLIC_WEATHER_API_KEY}&q=${city}`
+            );
+            if (response.ok) {
+                const data = await response.json();
+                setWeatherData(data);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        getLocationPermission();
+    }, []);
+
+    useEffect(() => {
+        reverseGeo();
+    }, [location]);
+
+    useEffect(() => {
+        getWeatherData();
+    }, [city]);
+
+    console.log(weatherData);
+
     return (
         <ScrollView className="flex-1">
             <LinearGradient
@@ -23,7 +85,7 @@ const HomeScreen = ({ navigation }) => {
                     }}
                 />
                 <View className="px-5 mt-8">
-                    <WeatherCard />
+                    <WeatherCard weatherData={weatherData} />
                     <View className="mt-5">
                         <MiniWeatherCard />
                     </View>
