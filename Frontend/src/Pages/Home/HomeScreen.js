cd8f2ca8a512b9f92b8c155346020021f69a56b7
vk-develops <vimalkumarv291@gmail.com>
2024-03-30 16:52:05 +0530

    chore: Removed unbwanted things

--- Frontend/src/Pages/Home/HomeScreen.js
+++ Frontend/src/Pages/Home/HomeScreen.js
@@ -1,33 +1,11 @@
-import { Button, ScrollView, View } from "react-native";
+import { ScrollView, View } from "react-native";
 import React from "react";
 import { LinearGradient } from "expo-linear-gradient";
-import { useLogoutMutation } from "../../Redux/Services/usersAuthApiSlice";
-import { useDispatch, useSelector } from "react-redux";
-import { removeCredentials } from "../../Redux/Features/usersAuthSlice";
 import WeatherCard from "../../Components/WeatherCard";
 import Header from "../../Components/Header";
 import MiniWeatherCard from "../../Components/MiniWeatherCard";
 
 const HomeScreen = ({ navigation }) => {
-    const { user, isAuthenticated } = useSelector((state) => state.auth);
-
-    console.log(user);
-    console.log(isAuthenticated);
-
-    const dispatch = useDispatch();
-
-    const [logout] = useLogoutMutation();
-
-    const logoutHandler = async () => {
-        try {
-            const response = await logout().unwrap();
-            console.log(response);
-            dispatch(removeCredentials());
-        } catch (err) {
-            console.log(err.message);
-        }
-    };
-
     return (
         <ScrollView className="flex-1">
             <LinearGradient
