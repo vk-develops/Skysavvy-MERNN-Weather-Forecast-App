915ff1b4ecbfc8b00f8751052329e80c8ee0b0b6
vk-develops <vimalkumarv291@gmail.com>
2024-03-30 17:04:40 +0530

    feat: Added the fucntionality to get the users location

--- Frontend/src/Pages/Home/HomeScreen.js
+++ Frontend/src/Pages/Home/HomeScreen.js
@@ -1,11 +1,58 @@
 import { ScrollView, View } from "react-native";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import * as Location from "expo-location";
 import { LinearGradient } from "expo-linear-gradient";
 import WeatherCard from "../../Components/WeatherCard";
 import Header from "../../Components/Header";
 import MiniWeatherCard from "../../Components/MiniWeatherCard";
 
 const HomeScreen = ({ navigation }) => {
+    const [location, setLocation] = useState(null);
+    const [address, setAddress] = useState(null);
+
+    const getLocationPermission = async () => {
+        let { status } = await Location.requestForegroundPermissionsAsync();
+
+        if (status !== "granted") {
+            console.log("Location permission not granted");
+        } else {
+            try {
+                const currentLocation = await Location.getCurrentPositionAsync(
+                    {}
+                );
+                setLocation(currentLocation);
+                console.log(currentLocation);
+            } catch (error) {
+                console.error("Error fetching location:", error);
+            }
+        }
+    };
+
+    const reverseGeo = async () => {
+        if (location) {
+            try {
+                const { coords } = location;
+                const reverseGeoCodeAddress =
+                    await Location.reverseGeocodeAsync({
+                        latitude: coords.latitude,
+                        longitude: coords.longitude,
+                    });
+                setAddress(reverseGeoCodeAddress);
+                console.log(reverseGeoCodeAddress);
+            } catch (error) {
+                console.error("Error reverse geocoding:", error);
+            }
+        }
+    };
+
+    useEffect(() => {
+        getLocationPermission();
+    }, []);
+
+    useEffect(() => {
+        reverseGeo();
+    }, [location]);
+
     return (
         <ScrollView className="flex-1">
             <LinearGradient
