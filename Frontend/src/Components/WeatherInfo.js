625491d377a75d03df8e343f0590466241a2c6d3
vk-develops <vimalkumarv291@gmail.com>
2024-03-31 11:04:24 +0530

    feat: Added the mini info card UI

--- Frontend/src/Components/WeatherInfo.js
+++ Frontend/src/Components/WeatherInfo.js
@@ -3,8 +3,53 @@ import React from "react";
 
 const WeatherInfo = () => {
     return (
-        <View>
-            <Text>WeatherInfo</Text>
+        <View
+            style={{ backgroundColor: "rgba(255, 255, 255, 0.2)" }}
+            className="flex items-center justify-between flex-row p-6 rounded-lg mt-4"
+        >
+            <View>
+                <Text
+                    style={{ fontFamily: "plexRegular" }}
+                    className="text-[15px] text-slate-300 text-center"
+                >
+                    Wind
+                </Text>
+                <Text
+                    style={{ fontFamily: "plexMedium" }}
+                    className="text-[24px] text-white text-center"
+                >
+                    245
+                </Text>
+            </View>
+
+            <View>
+                <Text
+                    style={{ fontFamily: "plexRegular" }}
+                    className="text-[15px] text-slate-300 text-center"
+                >
+                    Temperature
+                </Text>
+                <Text
+                    style={{ fontFamily: "plexMedium" }}
+                    className="text-[24px] text-white text-center"
+                >
+                    24&deg;
+                </Text>
+            </View>
+            <View>
+                <Text
+                    style={{ fontFamily: "plexRegular" }}
+                    className="text-[15px] text-slate-300 text-center"
+                >
+                    Humidity
+                </Text>
+                <Text
+                    style={{ fontFamily: "plexMedium" }}
+                    className="text-[24px] text-white text-center"
+                >
+                    24%
+                </Text>
+            </View>
         </View>
     );
 };
