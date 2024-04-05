edbf20c72950580a74f929b23fb33e3ce9723f49
vk-develops <vimalkumarv291@gmail.com>
2024-04-05 20:18:23 +0530

    chore: Modified cretain part of the codes

--- Frontend/src/Pages/Home/HomeScreen.js
+++ Frontend/src/Pages/Home/HomeScreen.js
@@ -5,6 +5,7 @@ import { LinearGradient } from "expo-linear-gradient";
 import WeatherCard from "../../Components/WeatherCard";
 import Header from "../../Components/Header";
 import MiniWeatherCard from "../../Components/MiniWeatherCard";
+import useGetWeatherData from "../../Hooks/useGetWeatherData";
 
 const HomeScreen = ({ navigation }) => {
     const [location, setLocation] = useState(null);
@@ -12,34 +13,7 @@ const HomeScreen = ({ navigation }) => {
     const [city, setCity] = useState(null);
     const [weatherData, setWeatherData] = useState(null);
 
-    const getLocationPermission = async () => {
-        let { status } = await Location.requestForegroundPermissionsAsync();
-
-        if (status !== "granted") {
-            console.log("Location not granted");
-            setCity("Mumbai");
-        } else {
-            let getlocation = await Location.getCurrentPositionAsync({});
-            setLocation(getlocation);
-        }
-    };
-
-    const reverseGeo = async () => {
-        if (location) {
-            const { coords } = location;
-            const reverseGeoCodeAddress = await Location.reverseGeocodeAsync({
-                latitude: coords.latitude,
-                longitude: coords.longitude,
-            });
-            setAddress(reverseGeoCodeAddress);
-
-            if (reverseGeoCodeAddress) {
-                setCity(reverseGeoCodeAddress[0].city);
-            }
-        } else {
-            console.log("Location is null or undefined");
-        }
-    };
+    const mainURL = `${process.env.EXPO_PUBLIC_WEATHER_API_CURRENT}?key=${process.env.EXPO_PUBLIC_WEATHER_API_KEY}&q=${city}`;
 
     const getWeatherData = async () => {
         try {
@@ -56,15 +30,50 @@ const HomeScreen = ({ navigation }) => {
     };
 
     useEffect(() => {
+        const getLocationPermission = async () => {
+            let { status } = await Location.requestForegroundPermissionsAsync();
+
+            if (status !== "granted") {
+                console.log("Location not granted");
+                setCity("Mumbai");
+            } else {
+                let getlocation = await Location.getCurrentPositionAsync({});
+                setLocation(getlocation);
+            }
+        };
+
         getLocationPermission();
     }, []);
 
     useEffect(() => {
+        const reverseGeo = async () => {
+            if (location) {
+                const { coords } = location;
+                const reverseGeoCodeAddress =
+                    await Location.reverseGeocodeAsync({
+                        latitude: coords.latitude,
+                        longitude: coords.longitude,
+                    });
+                setAddress(reverseGeoCodeAddress);
+
+                if (reverseGeoCodeAddress) {
+                    setCity(reverseGeoCodeAddress[0].city);
+                }
+            } else {
+                console.log("Location is null or undefined");
+            }
+        };
+
         reverseGeo();
     }, [location]);
 
     useEffect(() => {
         getWeatherData();
+        // const { data, isLoading } = useGetWeatherData(mainURL);
+
+        // if (data) {
+        //     setWeatherData(data);
+        // }
     }, [city]);
 
     return (
