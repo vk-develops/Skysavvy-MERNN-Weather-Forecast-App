fbceebc20954701832144720647830504784254c
vk-develops <vimalkumarv291@gmail.com>
2024-04-05 20:21:06 +0530

    feat: Added the optimization to the main card weather data api fetch

--- Frontend/src/Pages/Home/HomeScreen.js
+++ Frontend/src/Pages/Home/HomeScreen.js
@@ -1,4 +1,4 @@
-import { ScrollView, Text, View } from "react-native";
+import { ActivityIndicator, ScrollView, Text, View } from "react-native";
 import React, { useState, useEffect } from "react";
 import * as Location from "expo-location";
 import { LinearGradient } from "expo-linear-gradient";
@@ -12,8 +12,6 @@ const HomeScreen = ({ navigation }) => {
     const [address, setAddress] = useState(null);
     const [city, setCity] = useState(null);
     const [weatherData, setWeatherData] = useState(null);
-
-    const mainURL = `${process.env.EXPO_PUBLIC_WEATHER_API_CURRENT}?key=${process.env.EXPO_PUBLIC_WEATHER_API_KEY}&q=${city}`;
 
     const getWeatherData = async () => {
         try {
@@ -67,14 +65,23 @@ const HomeScreen = ({ navigation }) => {
         reverseGeo();
     }, [location]);
 
-    useEffect(() => {
-        getWeatherData();
-        // const { data, isLoading } = useGetWeatherData(mainURL);
+    const mainURL = `${process.env.EXPO_PUBLIC_WEATHER_API_CURRENT}?key=${process.env.EXPO_PUBLIC_WEATHER_API_KEY}&q=${city}`;
+    const { data: weatherDataFromHook, isLoading } = useGetWeatherData(mainURL);
 
-        // if (data) {
-        //     setWeatherData(data);
-        // }
-    }, [city]);
+    useEffect(() => {
+        if (weatherDataFromHook) {
+            setWeatherData(weatherDataFromHook);
+        }
+    }, [weatherDataFromHook]);
+
+    // useEffect(() => {
+    //     getWeatherData();
+    //     // const { data, isLoading } = useGetWeatherData(mainURL);
+
+    //     // if (data) {
+    //     //     setWeatherData(data);
+    //     // }
+    // }, [city]);
 
     return (
         <ScrollView className="flex-1">
@@ -92,6 +99,7 @@ const HomeScreen = ({ navigation }) => {
                     }}
                 />
                 <View className="px-5 mt-8">
+                    {isLoading && <ActivityIndicator />}
                     {weatherData && (
                         <View>
                             <WeatherCard
