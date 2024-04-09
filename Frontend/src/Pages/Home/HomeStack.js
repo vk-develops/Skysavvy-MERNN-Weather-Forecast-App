bd939558ac97aa1e3c9899961adf491282337aaa
vk-develops <vimalkumarv291@gmail.com>
2024-04-09 20:13:17 +0530

    feat: Added the custom header styles on the weather detail screen

--- Frontend/src/Pages/Home/HomeStack.js
+++ Frontend/src/Pages/Home/HomeStack.js
@@ -3,12 +3,14 @@ import {
     createStackNavigator,
     CardStyleInterpolators,
 } from "@react-navigation/stack";
+import { Ionicons } from "@expo/vector-icons";
 import HomeScreen from "./HomeScreen";
 import TopBarNavigator from "../../Navigation/TopBarNavigator";
+import { TouchableOpacity } from "react-native";
 
 const Stack = createStackNavigator();
 
-const HomeStack = () => {
+const HomeStack = ({ navigation }) => {
     return (
         <Stack.Navigator
             screenOptions={{
@@ -25,6 +27,26 @@ const HomeStack = () => {
                 component={HomeScreen}
             />
             <Stack.Screen
+                options={({ route }) => ({
+                    headerTitle: route.params.locName,
+                    headerTitleStyle: {
+                        fontFamily: "plexMedium",
+                        fontSize: 18,
+                    },
+                    headerTitleAlign: "center",
+                    headerLeft: () => (
+                        <TouchableOpacity
+                            onPress={() => navigation.goBack()}
+                            className="ml-[10px]"
+                        >
+                            <Ionicons
+                                name="chevron-back"
+                                size={24}
+                                color="black"
+                            />
+                        </TouchableOpacity>
+                    ),
+                })}
                 name="WeatherDetailScreen"
                 component={TopBarNavigator}
             />
