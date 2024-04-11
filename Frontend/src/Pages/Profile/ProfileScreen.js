54454593ce553a09edf3776dec696877ca565a91
vk-develops <vimalkumarv291@gmail.com>
2024-04-11 20:33:49 +0530

    feat: Added the profile element

--- Frontend/src/Pages/Profile/ProfileScreen.js
+++ Frontend/src/Pages/Profile/ProfileScreen.js
@@ -1,7 +1,47 @@
-import { ScrollView, Text, TouchableOpacity, View } from "react-native";
+import {
+    Dimensions,
+    Image,
+    ScrollView,
+    Text,
+    TouchableOpacity,
+    View,
+} from "react-native";
 import React from "react";
 import { LinearGradient } from "expo-linear-gradient";
 import useLogout from "../../Hooks/useLogout";
+
+const width = Dimensions.get("window").width;
+
+const ProfileElement = () => {
+    const img = {
+        uri: "https://www.translitescaffolding.com/wp-content/uploads/2013/06/user-avatar.png",
+    };
+
+    return (
+        <View className="relative flex items-center justify-center">
+            <View
+                style={{
+                    backgroundColor: "transparent",
+                    borderStyle: "solid",
+                    borderRightWidth: width,
+                    borderTopWidth: width / 1.2,
+                    borderRightColor: "transparent",
+                    borderTopColor: "rgba(255, 255, 255, 0.3)",
+                }}
+                className={`h-0 w-0 bg-transparent`}
+            ></View>
+            <View
+                style={{ width: 140, height: 140, backgroundColor: "#facc15" }}
+                className="absolute mt-[185px] flex items-center justify-center rounded-full shadow-lg drop-shadow-lg"
+            >
+                <Image
+                    className="w-36 h-36"
+                    source={img}
+                />
+            </View>
+        </View>
+    );
+};
 
 const ProfileScreen = () => {
     const { logoutHandler } = useLogout();
@@ -20,6 +60,7 @@ const ProfileScreen = () => {
                 className="min-h-screen"
                 colors={["#23227B", "#000236"]}
             >
+                <ProfileElement />
                 <View className="mt-12">
                     <TouchableOpacity
                         onPress={handleLogout}
