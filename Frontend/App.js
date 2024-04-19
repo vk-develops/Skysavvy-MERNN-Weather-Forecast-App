d131d541245164e83efeb6ab87705a88dc1d9cc1
vk-develops <vimalkumarv291@gmail.com>
2024-04-19 22:26:05 +0530

    feat: Added the splash screen prevention for 2 seconds

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,6 +1,7 @@
 import { useFonts } from "expo-font";
 import { StatusBar } from "expo-status-bar";
 import { Provider } from "react-redux";
+import * as SplashScreen from "expo-splash-screen";
 import Toast from "react-native-toast-message";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { NavigationContainer } from "@react-navigation/native";
@@ -8,6 +9,9 @@ import store from "./src/Redux/store";
 import MainNavigator from "./src/Navigation/MainNavigator";
 
 export default function App() {
+    SplashScreen.preventAutoHideAsync();
+    setTimeout(SplashScreen.hideAsync, 2000);
+
     const [fontsLoaded] = useFonts({
         plexRegular: require("./assets/fonts/IBMPlexSans-Regular.ttf"),
         plexMedium: require("./assets/fonts/IBMPlexSans-Medium.ttf"),
